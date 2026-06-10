0564abbc581483212a930980d13e81622f5a6ae1
TOUMIK HAQUE <toumikhaque@gmail.com>
2026-06-10 18:45:19 +0530

    Fix path of model/User.js

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -1,4 +1,4 @@
-import User from "../models/User"
+import User from "../models/User.js"
 import ApprovedStudent from "../models/ApprovedStudent.js"
 
 const getStudentDetails = async (req, res) => {
