8540fa530c029ad6b3a18e3a0350bf129afeba59
TOUMIK HAQUE <toumikhaque@gmail.com>
2026-06-10 13:37:19 +0530

    Fix IPv6 + email SMTP config

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -44,6 +44,10 @@ const sendOtp = async (req, res) => {
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
+      },
+      secure: true,
+      tls: {
+        rejectUnauthorized: false
       }
     })
 
--- server/server.js
+++ server/server.js
@@ -1,3 +1,6 @@
+import dns from "dns";
+dns.setDefaultResultOrder("ipv4first");
+
 const express = require('express')
 const cors = require('cors')
 const dotenv = require('dotenv')
