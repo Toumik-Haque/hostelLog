4c5e35cba188fe1ff08c9664028ae099c53904e8
TOUMIK HAQUE <toumikhaque@gmail.com>
2026-06-10 18:30:59 +0530

    Fix file path

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ dns.setDefaultResultOrder("ipv4first");
 import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
-import connectDB from "./config/db"
+import connectDB from "./config/db.js"
 
 dotenv.config()
 
@@ -16,25 +16,25 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-import approvedStudentRoutes from "./routes/approvedStudentRoutes"
+import approvedStudentRoutes from "./routes/approvedStudentRoutes.js"
 app.use(
   '/api/approved-students',
   approvedStudentRoutes
 )
 
-import adminAuthRoutes from "./routes/adminAuthRoutes"
+import adminAuthRoutes from "./routes/adminAuthRoutes.js"
 app.use('/api/admin-auth', adminAuthRoutes)
 
-import authRoutes from "./routes/authRoutes"
+import authRoutes from "./routes/authRoutes.js"
 app.use('/api/auth', authRoutes)
 
-import hostelRoutes from "./routes/hostelRoutes"
+import hostelRoutes from "./routes/hostelRoutes.js"
 app.use('/api/hostel', hostelRoutes)
 
-import adminRoutes from "./routes/adminRoutes"
+import adminRoutes from "./routes/adminRoutes.js"
 app.use('/api/admin', adminRoutes)
 
-import studentRoutes from "./routes/studentRoutes"
+import studentRoutes from "./routes/studentRoutes.js"
 app.use('/api/student', studentRoutes)
 
 app.get('/', (req, res) => {
