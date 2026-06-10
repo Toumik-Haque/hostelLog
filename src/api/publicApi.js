6c9ca5705f3fd05e02350ea4ea0262726fa6eac9
TOUMIK HAQUE <toumikhaque@gmail.com>
2026-06-10 12:50:38 +0530

    Fix wwrong API calling

--- src/api/publicApi.js
+++ src/api/publicApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const publicApi = axios.create({
-  baseURL: 'http://https://hostellog-api.onrender.com/api'
+  baseURL: 'http://hostellog-api.onrender.com/api'
 })
 
 export default publicApi
