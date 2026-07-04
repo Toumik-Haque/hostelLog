b38222fc88ca7ee40b0ea247781cc45e6c2dc749
TOUMIK HAQUE <toumikhaque@gmail.com>
2026-07-04 18:43:58 +0530

    Modify Banking

--- src/pages/BankAccount.jsx
+++ src/pages/BankAccount.jsx
@@ -34,87 +34,86 @@ function BankAccount() {
         <div className='h-100 overflow-auto py-3 hide-scrollbar'>
             <div className='mx-4'>
 
-                <div className='mx-4 text-center mb-4'>
-                    <div className='fw-bold fs- border-official color-official border-bottom border-dark pb-1'>AGEMC BOYS HOSTEL II</div>
+                <div className='fw-bold text-center color-official mb-2'>AGEMC BOYS HOSTEL II</div>
 
-                    <div className='mx- mt- d-flex justify-content-between border-bottom border-dark pb-1'>
-                        <div className='d-flex gap-2 align-items-center'>
+                <div className='px-3 py-3 card shadow-sm border-0 card1 rounded-3 text-center mb-3'>
+
+                    <div className='d-flex justify-content-between border-bottom border-official pb-1 mb-1'>
+                        <div className='d-flex fw-medium gap-1 align-items-center'>
                             <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-cash-stack" viewBox="0 0 16 16">
                                 <path d="M1 3a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1zm7 8a2 2 0 1 0 0-4 2 2 0 0 0 0 4" />
                                 <path d="M0 5a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1zm3 0a2 2 0 0 1-2 2v4a2 2 0 0 1 2 2h10a2 2 0 0 1 2-2V7a2 2 0 0 1-2-2z" />
                             </svg>
-                            <p className='m-0'>Fees of JUL'26 to DEC'26</p>
+                            <p className='m-0'>Fees of Jul'26 to Dec'26</p>
                         </div>
                         <p className='m-0 fw-bold color-official'>₹13500</p>
                     </div>
 
-                    <div className='d-flex justify-content-between text-secondary'>
+
+
+                    <div className='d-flex small justify-content-between text-secondary'>
                         <p className='m-0'>Hostel Seat Rent</p>
                         <p className='m-0'>₹1000</p>
                     </div>
 
-                    <div className='d-flex justify-content-between text-secondary'>
+                    <div className='d-flex small justify-content-between text-secondary'>
                         <p className='m-0'>Electricity Charges</p>
                         <p className='m-0'>₹500</p>
                     </div>
 
-                    <div className='d-flex justify-content-between text-secondary border-bottom border-dark pb-1'>
+                    <div className='d-flex small justify-content-between text-secondary'>
                         <p className='m-0'>Mess Fees</p>
                         <p className='m-0'>₹12000</p>
                     </div>
 
-                    <div className='fw-medium mb-3 mt-4 fs-4'>SCAN & PAY</div>
-                    <img src={QRCode} alt="QR Code" srcset="" />
-                    <div className='fs-small text-center text-secondary mt-3'>SBI - KAMKHYAGURI BRANCH</div>
+
+
                 </div>
 
-                <div className='mx-4 d-flex flex-column gap-2'>
+                <div className='d-flex flex-column gap-2 mb-4'>
 
-                    <div className='d-flex justify-content-between'>
-                        <div className='d-flex flex-column'>
-                            <p className='m-0 text-secondary'>UPI ID</p>
-                            <p className='m-0 fw-medium'>agecboyshosii@sbi</p>
-                        </div>
-                        <div className='d-flex align-items-end'>
-                            <button onClick={copyUPI} className='btn p-0 color-official d-flex gap-1 align-items-end'>
-                                <p className='m-0'>Copy</p>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-copy" viewBox="0 0 16 16">
-                                    <path fillRule="evenodd" d="M4 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 5a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-1h1v1a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h1v1z" />
-                                </svg>
-                            </button>
-                        </div>
-                    </div>
-
-                    <div className='d-flex justify-content-between'>
-                        <div className='d-flex flex-column'>
-                            <p className='m-0 text-secondary'>Account No</p>
+                    <div>
+                        <p className='m-0 text-secondary small'>Account No</p>
+                        <div className='d-flex gap-2'>
                             <p className='m-0 fw-medium'>44665967145</p>
-                        </div>
-                        <div className='d-flex align-items-end'>
-                            <button onClick={copyACNo} className='btn p-0 color-official d-flex gap-1 align-items-end'>
-                                <p className='m-0'>Copy</p>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-copy" viewBox="0 0 16 16">
+                            <button onClick={copyACNo} className='btn p-0 color-official d-flex align-items-center'>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-copy" viewBox="0 0 16 16">
                                     <path fillRule="evenodd" d="M4 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 5a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-1h1v1a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h1v1z" />
                                 </svg>
                             </button>
                         </div>
                     </div>
 
-                    <div className='d-flex justify-content-between'>
-                        <div className='d-flex flex-column'>
-                            <p className='m-0 text-secondary'>IFSC</p>
+                    <div>
+                        <p className='m-0 text-secondary small'>IFSC</p>
+                        <div className='d-flex gap-2'>
                             <p className='m-0 fw-medium'>SBIN0012409</p>
-                        </div>
-                        <div className='d-flex align-items-end'>
-                            <button onClick={copyIFSC} className='btn p-0 color-official d-flex gap-1 align-items-end'>
-                                <p className='m-0'>Copy</p>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-copy" viewBox="0 0 16 16">
+                            <button onClick={copyIFSC} className='btn p-0 color-official d-flex align-items-center'>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-copy" viewBox="0 0 16 16">
                                     <path fillRule="evenodd" d="M4 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 5a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-1h1v1a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h1v1z" />
                                 </svg>
                             </button>
                         </div>
                     </div>
 
+                    <div>
+                        <p className='m-0 text-secondary small'>UPI ID</p>
+                        <div className='d-flex gap-2'>
+                            <p className='m-0 fw-medium'>agecboyshosii@sbi</p>
+                            <button onClick={copyUPI} className='btn p-0 color-official d-flex align-items-center'>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-copy" viewBox="0 0 16 16">
+                                    <path fillRule="evenodd" d="M4 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 5a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-1h1v1a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h1v1z" />
+                                </svg>
+                            </button>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className='mx-4 border border-official text-center rounded p-2'>
+                    <div className='fw-medium mb-2 fs-5'>SCAN & PAY</div>
+                    <img src={QRCode} alt="QR Code" srcset="" style={{ height: '175px' }} />
+                    <div className='fs-small text-center text-secondary mt-2'>SBI - KAMKHYAGURI BRANCH</div>
                 </div>
 
             </div>
--- src/pages/StudentDashboard.jsx
+++ src/pages/StudentDashboard.jsx
@@ -94,16 +94,16 @@ export default function StudentDashboard() {
               }`}>
 
               {activeTab === 'status'
-                ? <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-house-door-fill" viewBox="0 0 16 16">
+                ? <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" className="bi bi-house-door-fill" viewBox="0 0 16 16">
                   <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5" />
                 </svg>
-                : <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-house-door" viewBox="0 0 16 16">
+                : <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" className="bi bi-house-door" viewBox="0 0 16 16">
                   <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293zM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4z" />
                 </svg>
               }
 
             </div>
-            <small className={`fw-medium transition ${activeTab === 'status'
+            <small className={`fw-medium mt-1 transition ${activeTab === 'status'
               ? 'activeTabText'
               : ''
               }`}>Home</small>
@@ -118,16 +118,16 @@ export default function StudentDashboard() {
               }`}>
 
               {activeTab === 'students'
-                ? <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-people-fill" viewBox="0 0 16 16">
+                ? <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" className="bi bi-people-fill" viewBox="0 0 16 16">
                   <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6m-5.784 6A2.24 2.24 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.3 6.3 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1zM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5" />
                 </svg>
-                : <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-people" viewBox="0 0 16 16">
+                : <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" className="bi bi-people" viewBox="0 0 16 16">
                   <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1zm-7.978-1L7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002-.014.002zM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4m3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0M6.936 9.28a6 6 0 0 0-1.23-.247A7 7 0 0 0 5 9c-4 0-5 3-5 4q0 1 1 1h4.216A2.24 2.24 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816M4.92 10A5.5 5.5 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275ZM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0m3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4" />
                 </svg>
               }
 
             </div>
-            <small className={`fw-medium transition ${activeTab === 'students'
+            <small className={`fw-medium mt-1 transition ${activeTab === 'students'
               ? 'activeTabText'
               : ''
               }`}>Students</small>
@@ -141,15 +141,15 @@ export default function StudentDashboard() {
               : ''
               }`}>
               {activeTab === 'bank'
-                ? <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-pentagon-fill" viewBox="0 0 16 16">
+                ? <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" className="bi bi-pentagon-fill" viewBox="0 0 16 16">
                   <path d="M7.685.256a.5.5 0 0 1 .63 0l7.421 6.03a.5.5 0 0 1 .162.538l-2.788 8.827a.5.5 0 0 1-.476.349H3.366a.5.5 0 0 1-.476-.35L.102 6.825a.5.5 0 0 1 .162-.538l7.42-6.03Z" />
                 </svg>
-                : <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-pentagon" viewBox="0 0 16 16">
+                : <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" className="bi bi-pentagon" viewBox="0 0 16 16">
                   <path d="M7.685 1.545a.5.5 0 0 1 .63 0l6.263 5.088a.5.5 0 0 1 .161.539l-2.362 7.479a.5.5 0 0 1-.476.349H4.099a.5.5 0 0 1-.476-.35L1.26 7.173a.5.5 0 0 1 .161-.54l6.263-5.087Zm8.213 5.28a.5.5 0 0 0-.162-.54L8.316.257a.5.5 0 0 0-.631 0L.264 6.286a.5.5 0 0 0-.162.538l2.788 8.827a.5.5 0 0 0 .476.349h9.268a.5.5 0 0 0 .476-.35l2.788-8.826Z" />
                 </svg>
               }
             </div>
-            <small className={`fw-medium transition ${activeTab === 'bank'
+            <small className={`fw-medium mt-1 transition ${activeTab === 'bank'
               ? 'activeTabText'
               : ''
               }`}>Banking</small>
@@ -163,15 +163,15 @@ export default function StudentDashboard() {
               : ''
               }`}>
               {activeTab === 'profile'
-                ? <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-person-fill" viewBox="0 0 16 16">
+                ? <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" className="bi bi-person-fill" viewBox="0 0 16 16">
                   <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
                 </svg>
-                : <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-person" viewBox="0 0 16 16">
+                : <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" className="bi bi-person" viewBox="0 0 16 16">
                   <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z" />
                 </svg>
               }
             </div>
-            <small className={`fw-medium transition ${activeTab === 'profile'
+            <small className={`fw-medium mt-1 transition ${activeTab === 'profile'
               ? 'activeTabText'
               : ''
               }`}>Profile</small>
