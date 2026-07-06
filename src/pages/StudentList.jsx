1b8bc92535dacd1988529ce7c92a5334fa6c58de
TOUMIK HAQUE <toumikhaque@gmail.com>
2026-07-06 17:04:27 +0530

    add mark features

--- src/pages/StudentList.jsx
+++ src/pages/StudentList.jsx
@@ -7,8 +7,27 @@ export default function StudentList() {
     const [search, setSearch] = useState('')
     const [filter, setFilter] = useState('ALL')
 
+    const [markActive, setMarkActive] = useState(false)
+    const [markedStudents, setMarkedStudents] = useState({})
+
     const navigate = useNavigate()
+
     const [students, setStudents] = useState([])
+
+    const showMark = () => {
+        setMarkActive(true)
+        localStorage.setItem(
+            "markActive",
+            JSON.stringify(true)
+        )
+    }
+
+    const hideMark = () => {
+        setMarkActive(false)
+        localStorage.removeItem("markActive")
+        setMarkedStudents({})
+        localStorage.removeItem("markedStudents")
+    }
 
     const fetchData = async () => {
         try {
@@ -26,7 +45,51 @@ export default function StudentList() {
 
     useEffect(() => {
         fetchData()
+
+        const savedMarkActive = JSON.parse(
+            localStorage.getItem("markActive") || 'false'
+        )
+
+        setMarkActive(savedMarkActive)
+
+        const savedMarks = JSON.parse(
+            localStorage.getItem("markedStudents") || "{}"
+        )
+
+        setMarkedStudents(savedMarks)
+
     }, [])
+
+    const toggleMark = (studentId) => {
+
+        // copy previously existed in markedStudent to updatedMarks
+        const updatedMarks = {
+            ...markedStudents
+        }
+
+        // check is in updatedMarks or not
+        if (updatedMarks[studentId]) {
+
+            // if in, remove it
+            delete updatedMarks[studentId]
+
+        } else {
+
+            // if not in, add it
+            updatedMarks[studentId] = true
+
+        }
+
+        // change markedStudents with updatedMarks
+        setMarkedStudents(updatedMarks)
+
+        // store updatedMarks as string to local-storage in 'markedStudents' key
+        localStorage.setItem(
+            "markedStudents",
+            JSON.stringify(updatedMarks)
+        )
+
+    }
 
     const getCardClass = (status) => {
 
@@ -61,15 +124,52 @@ export default function StudentList() {
         >
 
             {/* Search */}
-            <div className="sticky-top mx-4 mb-3 rounded-5">
+            <div className="sticky-top mx-4 mb-3 d-flex gap-2">
 
-                <input
-                    type="text"
-                    className="form-control shadow-sm px-3 py-2 rounded-5"
-                    placeholder="Search by room number or name..."
-                    value={search}
-                    onChange={(e) => setSearch(e.target.value)}
-                />
+                <div className="position-relative flex-grow-1">
+
+                    <input
+                        type="text"
+                        className={`form-control shadow-sm px-3 py-2 rounded-0
+                            ${search && ('pe-5')}
+                        `}
+                        placeholder="Search by room no. or name..."
+                        value={search}
+                        onChange={(e) => setSearch(e.target.value)}
+                    />
+
+                    {search && (
+                        <button
+                            type="button"
+                            className="btn border-0 position-absolute top-50 end-0 translate-middle-y me-2 p-0"
+                            onClick={() => setSearch("")}
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="18"
+                                height="18"
+                                fill="currentColor"
+                                className="bi bi-x-circle-fill text-secondary"
+                                viewBox="0 0 16 16"
+                            >
+                                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293z" />
+                            </svg>
+                        </button>
+                    )}
+
+                </div>
+
+                {markActive === true ?
+                    <button className='btn bg-danger rounded text-white shadow-sm' onClick={hideMark}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-x-lg" viewBox="0 0 16 16">
+                            <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z" />
+                        </svg>
+                    </button>
+                    : <button className='btn bg-official rounded text-white shadow-sm' onClick={showMark}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-toggles" viewBox="0 0 16 16">
+                            <path d="M4.5 9a3.5 3.5 0 1 0 0 7h7a3.5 3.5 0 1 0 0-7zm7 6a2.5 2.5 0 1 1 0-5 2.5 2.5 0 0 1 0 5m-7-14a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5m2.45 0A3.5 3.5 0 0 1 8 3.5 3.5 3.5 0 0 1 6.95 6h4.55a2.5 2.5 0 0 0 0-5zM4.5 0h7a3.5 3.5 0 1 1 0 7h-7a3.5 3.5 0 1 1 0-7" />
+                        </svg>
+                    </button>}
 
             </div>
 
@@ -195,7 +295,10 @@ export default function StudentList() {
                             >
 
                                 <div
-                                    className={`card shadow-sm ${getCardClass(s.status)}`}
+                                    className={`card shadow-sm
+                                        ${getCardClass(s.status)}
+                                        ${markedStudents[s.id] && ('card2')}
+                                    `}
                                 >
 
                                     <div className="card-body">
@@ -212,7 +315,12 @@ export default function StudentList() {
                                                     </span>
                                                 </div>
 
-                                                <p className="m-0 d-flex gap-1 text-muted border-bottom border-secondary">
+                                                <p className={`m-0 d-flex gap-1 border-bottom
+                                                        ${markedStudents[s.id] ?
+                                                        'color-official border-official'
+                                                        : 'text-muted border-secondary'
+                                                    }
+                                                    `}>
 
                                                     {s.lastStatusChange
                                                         ? <span >from</span>
@@ -237,20 +345,43 @@ export default function StudentList() {
 
                                                 <h5>{s.name}</h5>
 
-                                                {s.status === 'UNREGISTERED'
-                                                    ? null
-                                                    : <div className='d-flex gap-3'>
-                                                        <p className='mb-1 d-flex gap-1 align-items-center'>
-                                                            <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" fill="currentColor" className="color-official bi bi-telephone-fill" viewBox="0 0 16 16">
-                                                                <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z" />
-                                                            </svg>
-                                                            {s.mobile}
-                                                        </p>
-                                                        <p className="mb-1">
-                                                            Room: {s.roomNo}
-                                                        </p>
+                                                <div className='d-flex justify-content-between'>
+                                                    {s.status === 'UNREGISTERED'
+                                                        ? null
+                                                        : <div className='d-flex gap-3'>
+                                                            <p className='mb-1 d-flex gap-1 align-items-center'>
+                                                                <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" fill="currentColor" className="color-official bi bi-telephone-fill" viewBox="0 0 16 16">
+                                                                    <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z" />
+                                                                </svg>
+                                                                {s.mobile}
+                                                            </p>
+                                                            <p className="mb-1">
+                                                                Room: {s.roomNo}
+                                                            </p>
+                                                        </div>
+                                                    }
+
+
+                                                    <div className={`d-flex justify-content-end
+                                                        ${s.status === 'UNREGISTERED' && ('w-100')}
+                                                    `}>
+                                                        {markActive === true ?
+
+                                                            <div style={{ cursor: "pointer" }} onClick={() => toggleMark(s.id)}>
+                                                                {markedStudents[s.id] ?
+                                                                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="color-official bi bi-toggle-on" viewBox="0 0 16 16">
+                                                                        <path d="M5 3a5 5 0 0 0 0 10h6a5 5 0 0 0 0-10zm6 9a4 4 0 1 1 0-8 4 4 0 0 1 0 8" />
+                                                                    </svg>
+                                                                    : <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="color-official bi bi-toggle-off" viewBox="0 0 16 16">
+                                                                        <path d="M11 4a4 4 0 0 1 0 8H8a5 5 0 0 0 2-4 5 5 0 0 0-2-4zm-6 8a4 4 0 1 1 0-8 4 4 0 0 1 0 8M0 8a5 5 0 0 0 5 5h6a5 5 0 0 0 0-10H5a5 5 0 0 0-5 5" />
+                                                                    </svg>
+                                                                }
+                                                            </div>
+
+                                                            : null
+                                                        }
                                                     </div>
-                                                }
+                                                </div>
 
                                                 {s.foodPreference === 'Veg' && (
                                                     <p className='mb-1'>
@@ -280,7 +411,7 @@ export default function StudentList() {
 
             </div>
 
-        </div>
+        </div >
 
     )
 }
